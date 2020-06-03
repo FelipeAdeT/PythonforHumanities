720bf28352528c12a97c21aba4d766a05173ab39
Felipe Alvarez de Toledo <Felipe@Felipes-MacBook-Pro.local>
2020-06-03 17:15:14 -0400

    Nico Edits

--- QuestionsBDT.py
+++ QuestionsBDT.py
@@ -1,14 +1,11 @@
 from Questions import *
 
-Q1 = Solution("type(year_of_birth) outputs 'str' (a string). We enclosed the variable in quotes, so Python has interpreted them not as a number, but as text. This sometimes desirable, but will also prevent us from conducting mathematical operations on its numerical value.")
+E1 = Solution("type(year_of_birth) outputs 'str' (a string). We enclosed the variable in quotes, so Python has interpreted them not as a number, but as text. This sometimes desirable, but will also prevent us from conducting mathematical operations on its numerical value.")
 
-Q2 = MultQuestion("How can you recognize that the string has been transformed into an integer?\n\na) The characters are digits \n\nb) The characters are not surrounded by quotes \n\nc) The characters are surrounded by quotes \n\nd) The characters are surrounded by digits\n\n","b")
+Q1 = MultQuestion("How can you recognize that the string has been transformed into an integer?\n\na) The characters are digits \n\nb) The characters are not surrounded by quotes \n\nc) The characters are surrounded by quotes \n\nd) The characters are surrounded by digits\n\n","b")
 
-Q3 = MultQuestion("Can we operate on these numbers directly?\n\na) No, Python gives us an error because one of these is not a number. \n\nb) Yes, Python allows us to subtract strings.\n\nb) No, Python does not allow us to subtract numbers.\n\nd)None of the above.\n\n","a", right='Correct! We must be careful with data types', wrong='Are you sure? Be mindful of data types')
+E2 = Solution("year_of_birth = int(year_of_birth)")
 
-Q4 = MultQuestion("What is the correct approach for figuring out the artist's life span?\n\na) Subtract year_of_birth from year_of_death \n\nb) Convert the variables to strings, then subtract\n\nc)Convert the year_of_death to a number, then subtract\n\nd)None of the above.\n\n","c")
+E3 = Solution("x = 1\ny = 2\n(x - y)**2/2")
 
-E1 = Solution("year_of_birth = int(year_of_birth)")
-
-E2 = Solution("(x-y)**2/2")
-E3 = Solution("lifespan = year_of_death - year_of_birth\nprint(lifespan)")
+E4 = Solution("lifespan = year_of_death - year_of_birth\nprint(lifespan)")
--- QuestionsConditionals.py
+++ QuestionsConditionals.py
@@ -1,21 +1,19 @@
 from Questions import *
 
-Q1 = Solution("artist = 'Frida Kahlo'\nlen(artist)>= 10")
+E1 = Solution("artist = 'Frida Kahlo'\nlen(artist)>= 10")
 
-Q2 = MultQuestion("Python is not making a qualitative argument about these artists' work. What is happening in the above comparisons?\n\na) Python is comparing the lengths of the strings\n\nb) Python is checking your computer's character tables, which place 'F' (in Frida) after 'D' but before 'P'\n\nc) Python is checking your computer's character tables, which place 'F' after 'P' but before 'D'\n\nd) None of the above\n\n", "b")
+E2= Solution("len(artist)>=10 and len(artist)<15")
 
-Q3= Solution("len(artist)>=10 and artist>'Diego Rivera'")
+E3= Solution("'Frida' in artist")
 
-Q4= Solution("'Frida' in artist")
+E4 =Solution("if len(artist)>=10:\n    print(artist+ ': '+ str(len(artist)) + ' letters')")
+             
+Q1 = MultQuestion("Say we run the following code. Which block will run and what will be the output?\n\nartist = 'Frida Kahlo\n\nif len(artist)==10:\n    print(artist+ ': 10 letters')\nelse:\n    print(artist+ ': '+ str(len(artist)) + ' letters')\n\na) The else block, 'Frida Kahlo: 10 letters'\n\nb) The if block, 'Frida Kahlo: 11 letters' \n\nc) The else block, 'artist: 10 letters' \n\nd) The else block, 'Frida Kahlo: 11 letters'\n\n","d")
+             
+E5 = Solution("if x<0:\n    print('x is less than 0')\nelif x<5:\n    print('x is less than 5')\nnelse:\n    print('x is greater than or equal to 5')")
+             
+E6 = Solution("if x<0:\n    print('x is less than 0')\nelif x<5:\n    print('x is less than 5')\nelif x<10:\n    print('x is less than 10')\nelse:\n    print('x is greater or equal to 10')")
 
-Q5 =Solution("if len(artist)>=10:\n    print(artist+ ': '+ str(len(artist)) + ' letters')")
              
-Q6 = MultQuestion("Say we run this code. Which block will run and what will be the output?\n\nif len(artist)==10:\n    print(artist+ ': 10 letters')\nelse:\n    print(artist+ ': '+ str(len(artist)) + ' letters')\n\na) The else block, 'Frida Kahlo: 10 letters'\n\nb) The if block, 'Frida Kahlo: 11 letters' \n\nc) The else block, 'artist: 10 letters' \n\nd) The else block, 'Frida Kahlo: 11 letters'\n\n","d")
+Q2 = MultQuestion("For x = 4, what would the following program output?\n\nif x<0:\n    print('x is less than 0')\n    if x<5:\n        print('x is less than 5')\nif x<10:\n    print('x is less than 10')\nelse:\n    print('x is greater than 10')\n\na)x is less than 5\nx is less than 10\n\nb) x is less than 10\n\nb)x is less than 0\nx is less than 5\nx is less than 10\n\nd)x is less than 5\n\n",'b')
              
-Q7 = Solution("if x<0:\n    print('x is less than 0')\nelif x<5:\n    print('x is less than 5')\nelif x<10:\n    print('x is less than 10')\nelse:\n    print('x is greater than 10')")
-             
-Q8 = MultQuestion("For x = -1, what would the following program's output?\n\nif x<5:\n    print('x is less than 5')\nelif x<0:\n    print('x is less than 0')\nelif x<10:\n    print('x is less than 10')\nelse:\n    print('x is greater than 10')\n\na)x is less than 5\n\nb) x is less than 10\n\nb)x is less than 5\nx is less than 10\n\nd)x is less than 0\n\n",'a')
-             
-Q9 = MultQuestion("For x = 4, what would the following program output?\n\nif x<0:\n    print('x is less than 0')\n    if x<5:\n        print('x is less than 5')\nif x<10:\n    print('x is less than 10')\nelse:\n    print('x is greater than 10')\n\na)x is less than 5\nx is less than 10\n\nb) x is less than 10\n\nb)x is less than 0\nx is less than 5\nx is less than 10\n\nd)x is less than 5\n\n",'b')
-             
-Q10= Solution("bandnumbers = []\nfor words in wordlist:\n    for token in words:\n        try:\n            bandnumbers.append(int(token))\n        except:\n            pass")
