fc3ba74637759070b0a13dc6e72980f77c287991
Felipe Alvarez de Toledo <Felipe@Felipes-MacBook-Pro.local>
2020-06-22 14:10:16 -0400

    Day's work Monday 22. Eric's Collections revisions. Some minor changes to other files.

---
+++ QuestionsPUYP.py
@@ -0,0 +1,4 @@
+from Questions import *
+
+E7= Solution("bandnumbers = []\nfor words in wordlist:\n    for token in words:\n        try:\n            bandnumbers.append(int(token))\n        except:\n            pass")
+
