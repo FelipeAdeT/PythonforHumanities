60f859acb49ad9c39279514651089ad7be4a9d70
Felipe Alvarez de Toledo <Felipe@Felipes-MacBook-Pro.local>
2020-06-30 09:48:28 -0400

    Fix Loops Questions

--- QuestionsLoops.py
+++ QuestionsLoops.py
@@ -4,8 +4,8 @@ E1 = Solution("Your code might look something like:\n\ncount = 6\n\nwhile count 
 
 E2 = Solution("from random import shuffle\ngame = ['duck', 'duck', 'duck', 'goose', 'duck']\n\nwhile game[0]=='duck':\n    print('safe')\n    shuffle(game)\n\nprint('Run!')")
 
-Q3 = Solution("value = 0\nwhile True:\n    if value%2==0:\n        print(value)\n    if value ==20:\n        break\n    value= value+1")
+E3 = Solution("value = 0\nwhile True:\n    if value%2==0:\n        print(value)\n    if value ==20:\n        break\n    value= value+1")
 
-Q4 = Solution("value = 0\n\nwhile value<=20:\n    value= value+1\n    if value%2==0:\n        print(value)\n")
+E4 = Solution("value = 0\n\nwhile value<=20:\n    value= value+1\n    if value%2==0:\n        print(value)\n")
 
-Q5 = Solution("You could use any term to refer to the items in the list, as long as you keep them consistent, and preferably, easy to understand. I chose 'donut'.\n\nMonutsDonuts = ['Classic Chocolate','Strawberry','Maple-Bacon','Plain Glazed', 'Raspberry','Lemon Glaze']\n\nfor donut  in  MonutsDonuts:\n    if 'Glaze' in donut:\n        print(donut)")
+E5 = Solution("You could use any term to refer to the items in the list, as long as you keep them consistent, and preferably, easy to understand. I chose 'donut'.\n\nMonutsDonuts = ['Classic Chocolate','Strawberry','Maple-Bacon','Plain Glazed', 'Raspberry','Lemon Glaze']\n\nfor donut  in  MonutsDonuts:\n    if 'Glaze' in donut:\n        print(donut)")
