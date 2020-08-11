0b89ab1621b1d08dd87c136ae4f4569b0d3093e1
Felipe Alvarez de Toledo <Felipe@Felipes-MacBook-Pro.local>
2020-08-11 13:16:44 -0400

    Add exercise 5

--- QuestionsFunctions.py
+++ QuestionsFunctions.py
@@ -6,4 +6,6 @@ E2 = Solution("print(a,b,c)\n\nLooking at the documentation, you will find that 
 
 E3 = Solution("""def liftoff(count=10):\n    while count > 0:\n        print(str(count) +"!")\n        count = count - 1\n    print("lift off!")""")
 
-E4 = Solution("""def liftoff(count=10):\n    a=''\n    while count > 0:\n        a = a + '\\n'+ str(count) +'!'\n        count = count - 1\n    return (a + 'Lift off!')""")
+E4 = Solution("""def liftoff(count=10):\n    a=''\n    while count > 0:\n        a = a + ' '+ str(count) +'!'\n        count = count - 1\n    return (a + 'Lift off!')""")
+
+E5= Solution("def distance (point1,point2):\n    xdistance = abs(point1[0]-point2[0])\n    ydistance = abs(point1[1]-point2[1])\n    return xdistance, ydistance\n\ndistancex, distancey = distance(point1,point2)")
